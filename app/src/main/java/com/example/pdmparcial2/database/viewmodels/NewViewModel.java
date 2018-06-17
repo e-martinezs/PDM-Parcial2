abb22742e3474a79ad3cadb92561033fdf64ef8f
emartinezs <00116316@uca.edu.sv>
2018-06-16 23:17:37 -0600

    Correccion de error de sintaxis

--- app/src/main/java/com/example/pdmparcial2/database/viewmodels/NewViewModel.java
+++ app/src/main/java/com/example/pdmparcial2/database/viewmodels/NewViewModel.java
@@ -6,7 +6,6 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
-import com.example.pdmparcial2.activities.MainActivity;
 import com.example.pdmparcial2.database.repositories.NewsRepository;
 import com.example.pdmparcial2.model.New;
 import com.example.pdmparcial2.model.User;
@@ -37,7 +36,7 @@ public class NewViewModel extends AndroidViewModel {
     public void insertNews(List<New> news, User user) {
         repository.insertNews(news, user);
     }
-w
+
     public void saveFavorite(String newId) {
         repository.setFavorite(newId, true);
     }
